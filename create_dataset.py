59ecb74590df87e01c592ff39829a8a625112e64
YESsPeace <damir200606@gmail.com>
2023-10-12 18:56:40 +0400

    code for creating dataset are completed

--- create_dataset.py
+++ create_dataset.py
@@ -1,8 +1,8 @@
 import os
+import pickle
 
 import mediapipe as mp
 import cv2
-import matplotlib.pyplot as plt
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -12,24 +12,32 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
 
+data = []  # dataset's data
+labels = []  # dataset's labels
+
 for dir_ in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         results = hands.process(img_rgb)
 
+        if results.multi_hand_landmarks:  # if we have any hands
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    z = hand_landmarks.landmark[i].z
 
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                img_rgb,  # image to draw
-                hand_landmarks,  # model output
-                mp_hands.HAND_CONNECTIONS,  # hand connections
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style(),
-            )
+                    data_aux.extend((x, y, z))
 
-        plt.figure()
-        plt.imshow(img_rgb)
+            data.append(data_aux)
+            labels.append(dir_)
 
-plt.show()
+# saving our dataset into .pickle file
+with open('dataset_dictionary.pickle', 'wb') as file:
+    pickle.dump({
+        'data': data,
+        'labels': labels,
+    }, file)
