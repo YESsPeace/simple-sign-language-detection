edf3803d0ba87e9f7412ea97d211e8e02160ad46
YESsPeace <damir200606@gmail.com>
2023-10-28 22:10:39 +0400

    Added help button

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,16 +1,17 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 # keyboard for client
-b_start = KeyboardButton('/start')
-b_info = KeyboardButton('/info')
-
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
+b_help = KeyboardButton('/help')
+b_info = KeyboardButton('/info')
+
+kb_client.add(b_help)
 kb_client.add(b_info)
 
 # keyboard for info message
-b_site = InlineKeyboardButton(text='Мой сайт', url='https://damir-ernazarov-yesspeace.carrd.co/')
-
 kb_info = InlineKeyboardMarkup(resize_keyboard=True)
+
+b_site = InlineKeyboardButton(text='Мой сайт', url='https://damir-ernazarov-yesspeace.carrd.co/')
 
 kb_info.insert(b_site)
