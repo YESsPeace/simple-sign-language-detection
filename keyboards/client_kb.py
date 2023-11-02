9f5057cfd3e2acc672bf02e55b8307511b789339
YESsPeace <damir200606@gmail.com>
2023-11-02 18:38:46 +0400

    Added new button below info message

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -12,6 +12,8 @@ kb_client.add(b_info)
 # keyboard for info message
 kb_info = InlineKeyboardMarkup(resize_keyboard=True)
 
+b_github = InlineKeyboardButton(text='Github', url='https://github.com/YESsPeace/simple-sign-language-recognition')
 b_site = InlineKeyboardButton(text='Мой сайт', url='https://damir-ernazarov-yesspeace.carrd.co/')
 
+kb_info.insert(b_github)
 kb_info.insert(b_site)
